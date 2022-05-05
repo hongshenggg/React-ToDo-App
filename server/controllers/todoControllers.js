9fcac690e313af834d097bf19d6ca783c07aeab6
Hong Sheng <lee_hong_sheng@hotmail.com>
2022-05-05 14:38:30 +0800

    Implemented initial REST API

--- server/controllers/todoControllers.js
+++ server/controllers/todoControllers.js
@@ -1,17 +1,45 @@
-function createTodo(req, res) {
+const todoModel = require('../models/todoModel');
+
+async function createTodo(req, res) {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error('Please input a title for the todo');
+  }
+  const todo = await todoModel.create({
+    title: req.body.title,
+    description: req.body.description
+  });
+
   res.status(200).json({message: 'created todo'})
 }
 
-function readTodos(req, res) {
-  res.status(200).json({message: 'read todos'})
+async function readTodos(req, res) {
+  const todos = await todoModel.find(); 
+  res.status(200).json(todos);
 }
 
-function updateTodo(req,res) {
-  res.status(200).json({message: 'updated todo'})
+async function updateTodo(req,res) {
+  const todo = await todoModel.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error('Todo not found');
+  }
+
+  const updatedTodo = await todoModel.findByIdAndUpdate(req.params.id,
+    req.body, {new: true});
+
+  res.status(200).json(updatedTodo);
 }
 
-function deleteTodo(req,res) {
-  res.status(200).json({message: 'delete todo'})
+async function deleteTodo(req,res) {
+  const todo = await todoModel.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error('Todo not found');
+  }
+
+  await todo.remove();
+  res.status(200).json({id: req.params.id});
 }
 
 module.exports = {createTodo, readTodos,updateTodo, deleteTodo}
---
+++ server/models/todoModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
