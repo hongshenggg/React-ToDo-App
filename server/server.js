da7afca7cad2e07b2781d91d228440d9cd451d74
Hong Sheng <lee_hong_sheng@hotmail.com>
2022-05-05 14:01:14 +0800

    Added MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
